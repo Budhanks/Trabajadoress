4dd38111365bbc31b1b43e619d73e78d25cb4556
Budhanks <eduperezferia@gmail.com>
2025-05-21 01:11:44 -0600

    login5

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const express = require('express');
 const path = require('path');
-const session = require('express-session');
-// connect-pg-simple
-const pgSession = require('connect-pg-simple')(session);
-const { pool } = require('./config/conexion');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
