54ef523ceb481c86a5b8a045eeed9a64af24687e
Budhanks <eduperezferia@gmail.com>
2025-05-21 01:19:55 -0600

    login6

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
